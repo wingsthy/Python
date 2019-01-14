2c1c02053277ad94a4494d68daabe901046680ef
wingsthy <wingsthy@163.com>
2019-01-14 21:38:57 +0800

    firstday

--- lxf_webapp/www/app.py
+++ lxf_webapp/www/app.py
@@ -3,4 +3,22 @@
 
 import logging; logging.basicConfig(level=logging.INFO)
 
-import 
+import asyncio, os, json, time 
+from datetime import datetime 
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>',headers={
+        'content-type':'text/html'
+        })
+
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv = yield from loop.create_server(app.make_handler(), '0.0.0.0', 80)
+    logging.info('server started at http://127.0.0.1:80...')
+    return srv 
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
